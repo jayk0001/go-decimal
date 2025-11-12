0a5cf7313f9eea933a6d0bf70c231f4e19c378d8
Jay <jayatbkrs@gmail.com>
2025-11-11 20:29:30 -0800

    added more comprehensive comment

--- main.go
+++ main.go
@@ -15,37 +15,43 @@ func main() {
 	// Create a new reader for standard input
 	reader := bufio.NewReader(os.Stdin)
 
-	// Read input until a newLine character is encountered
+	// Read input1 until a newLine character is encountered
 	input1, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
 	}
-	// Remove the newline character and any surrounding whitespace
+
+	// Remove the newline character and any surrounding whitespace,
+	// create decimal using user entered string input
 	first, _ := decimal.NewFromString(strings.TrimSpace(input1))
 
 	fmt.Print("Enter the second number: ")
 
-	// Read input until a newLine character is encountered
+	// Read input2 until a newLine character is encountered
 	input2, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
 	}
-	// Remove the newline character and any surrounding whitespace
+
+	// Remove the newline character and any surrounding whitespace,
+	// create decimal using user entered string input
 	second, _ := decimal.NewFromString(strings.TrimSpace(input2))
 
 	fmt.Print("Enter the operation(+, -, /, *): ")
 
-	// Read input until a newLine character is encountered
+	// Read input3 until a newLine character is encountered
 	input3, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
 	}
-	// Remove the newline character and any surrounding whitespace
+	// Remove the newline character and any surrounding whitespace, this will be operation
 	operation := strings.TrimSpace(input3)
 
+	// Using switch + case handling in golang, perform correct calculate operation
+	// from decimal library, return result after caculcate
 	switch operation {
 	case "+":
 		res := first.Add(second)

ed377c8c16f923e9a932c7884e16bcec8a3e4bb8
Jay <jayatbkrs@gmail.com>
2025-11-11 18:47:10 -0800

    test and getting used to four basic operations

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	price, err := decimal.NewFromString("136.02")
+	if err != nil {
+		panic(err)
+	}
+
+	qty := decimal.NewFromInt(3)
+
+	fee, _ := decimal.NewFromString(".035")
+	taxRate, _ := decimal.NewFromString("0.08875")
+
+	subTotal := price.Mul(qty)
+
+	preTax := subTotal.Mul(fee.Add(decimal.NewFromFloat(1)))
+
+	total := preTax.Mul(taxRate.Add(decimal.NewFromFloat(1)))
+
+	fmt.Println("SubTotal: ", subTotal)
+	fmt.Println("Pre-tax: ", preTax)
+	fmt.Println("Taxes: ", total.Sub(preTax))
+	fmt.Println("Total: ", total)
+	fmt.Println("Tax rate: ", total.Sub(preTax).Div(preTax))
 }
